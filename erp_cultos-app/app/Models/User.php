cb942d4ba497fcc6b37716ae534c76162b284cf7
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-05 22:21:26 +0200

    Some updates in system

--- erp_cultos-app/app/Models/User.php
+++ erp_cultos-app/app/Models/User.php
@@ -12,37 +12,22 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'surname',
         'function',
-        'teological_level',
+        'theological_level',
         'contact',
         'userType',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
