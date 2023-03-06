05d0de191fdc063356929c30ed4c7739a6a9d3a8
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-06 15:25:23 +0200

    Somee updates in controller

--- erp_cultos-app/app/Http/Controllers/shepherdController.php
+++ erp_cultos-app/app/Http/Controllers/shepherdController.php
@@ -3,11 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class shepherdController extends Controller
 {
     public function addShepherd()
     {
         return view('Admin.addShepherd');
+    }
+    public function storeShepherd(Request $request)
+    {       
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'userType' => $request->userType,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $user->attachRole('shepherd');
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect()->back();
+        
     }
 }
