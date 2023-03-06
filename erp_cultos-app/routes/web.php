8b639bb52193944f4c6d1eac4a94c1221a9cfaea
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-06 14:18:16 +0200

    Some updates in web.php

--- erp_cultos-app/routes/web.php
+++ erp_cultos-app/routes/web.php
@@ -3,11 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dashboardController;
 
+require __DIR__.'/auth.php';
+
 Route::get('/', function () {
     return view('auth.login');
 });
 
-//Inicio da rota que e responsavel por redirecionar os usuarios
-Route::get('/dashboard', [dashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
+//Inicio das rotas da parte de administrador
+Route::group(['prefix' => 'admin', 'middleware' => ['role:admin']], function() {
+    Route::get('/dashboard', [dashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
+});
