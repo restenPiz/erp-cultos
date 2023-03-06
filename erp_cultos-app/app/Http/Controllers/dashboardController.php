2a7999275797910509e898d05737682287b19937
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-06 13:19:52 +0200

    Some updates in dashboardController

--- erp_cultos-app/app/Http/Controllers/dashboardController.php
+++ erp_cultos-app/app/Http/Controllers/dashboardController.php
@@ -11,23 +11,23 @@ class dashboardController extends Controller
     public function index()
     {
         if(Auth::user()->hasRole('admin')){
-        
+            return view('Admin.Index');
         }
         else if(Auth::user()->hasRole('shepherd'))
         {
-
+            return 404;
         }
         else if(Auth::user()->hasRole('worship_leader'))
         {
-            
+            return 404;
         }
         else if(Auth::user()->hasRole('Member'))
         {
-            
+            return 404;
         }
         else if(Auth::user()->hasRole('treasurer'))
         {
-            
+            return 404;
         }
         else{
             return redirect()->back();
