8cd3ccd9c5be1d68d6df87051b79dd059f5bcf8f
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-07 14:41:56 +0200

    Some updates in Index Layout

--- erp_cultos-app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ erp_cultos-app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Support\Facades\Auth;
 class AuthenticatedSessionController extends Controller
 {
     public function create()
