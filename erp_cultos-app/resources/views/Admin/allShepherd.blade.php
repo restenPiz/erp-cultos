d4fd0dcd53bc1b297115d6cc6503874e047456f0
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-08 08:11:27 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Admin/allShepherd.blade.php
+++ erp_cultos-app/resources/views/Admin/allShepherd.blade.php
@@ -81,18 +81,18 @@
                                                         <div class="edit">
                                                             <button class="btn btn-sm btn-success edit-item-btn"
                                                                 data-bs-toggle="modal"
-                                                                data-bs-target="#showModal{{$user->id}}">Editar</button>
+                                                                data-bs-target="#showModal">Editar</button>
                                                         </div>
                                                         <div class="remove">
                                                             <button class="btn btn-sm btn-danger remove-item-btn"
                                                                 data-bs-toggle="modal"
-                                                                data-bs-target="#deleteRecordModal{{$user->id}}">Eliminar</button>
+                                                                data-bs-target="#deleteRecordModal{{ $user->id }}">Eliminar</button>
                                                         </div>
                                                     </div>
                                                 </td>
                                             </tr>
 
-                                            {{-- Inicio dos dois modais --}}
+                                            {{-- Inicio do modal para editar o pastor --}}
                                             <div class="modal fade" id="showModal{{$user->id}}" tabindex="-1"
                                                 aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                 <div class="modal-dialog modal-dialog-centered">
@@ -105,59 +105,63 @@
                                                         <form class="tablelist-form" autocomplete="off">
                                                             <div class="modal-body">
                                                                 <div class="mb-3" id="modal-id" style="display: none;">
-                                                                    <label for="id-field" class="form-label">Nome</label>
+                                                                    <label for="id-field" class="form-label">ID</label>
                                                                     <input type="text" id="id-field" class="form-control"
-                                                                        value="{{$user->name}}" readonly />
+                                                                        placeholder="ID" readonly />
                                                                 </div>
 
                                                                 <div class="mb-3">
-                                                                    <label for="customername-field" class="form-label">Apelido</label>
+                                                                    <label for="customername-field" class="form-label">Customer
+                                                                        Name</label>
                                                                     <input type="text" id="customername-field"
-                                                                        class="form-control" placeholder="Enter Name" value="{{$user->surname}}"
+                                                                        class="form-control" placeholder="Enter Name"
                                                                         required />
+                                                                    <div class="invalid-feedback">Please enter a customer name.
                                                                     </div>
                                                                 </div>
 
                                                                 <div class="mb-3">
                                                                     <label for="email-field" class="form-label">Email</label>
                                                                     <input type="email" id="email-field"
-                                                                        class="form-control" value="{{$user->email}}"
+                                                                        class="form-control" placeholder="Enter Email"
                                                                         required />
+                                                                    <div class="invalid-feedback">Please enter an email.</div>
                                                                 </div>
 
                                                                 <div class="mb-3">
-                                                                    <label for="phone-field" class="form-label">Contacto</label>
+                                                                    <label for="phone-field" class="form-label">Phone</label>
                                                                     <input type="text" id="phone-field"
-                                                                        class="form-control" value="{{$user->contact}}"
+                                                                        class="form-control" placeholder="Enter Phone no."
                                                                         required />
                                                                     <div class="invalid-feedback">Please enter a phone.</div>
                                                                 </div>
 
                                                                 <div class="mb-3">
-                                                                    <label for="date-field" class="form-label">Funcao</label>
+                                                                    <label for="date-field" class="form-label">Joining
+                                                                        Date</label>
                                                                     <input type="text" id="date-field"
-                                                                        class="form-control" value="{{$user->function}}"
+                                                                        class="form-control" placeholder="Select Date"
                                                                         required />
                                                                     <div class="invalid-feedback">Please select a date.</div>
                                                                 </div>
 
                                                                 <div>
                                                                     <label for="status-field"
-                                                                        class="form-label">Nivel Teologico</label>
+                                                                        class="form-label">Status</label>
                                                                     <select class="form-control" data-trigger
                                                                         name="status-field" id="status-field" required>
-                                                                        <option value="{{$user->theological_level}}">{{$user->theological_level}}</option>
-                                                                        <option value="Caminho de Deus">Caminho de Deus</option>
-                                                                        <option value="Caminho de Joao">Caminho de Joao</option>
+                                                                        <option value="">Status</option>
+                                                                        <option value="Active">Active</option>
+                                                                        <option value="Block">Block</option>
                                                                     </select>
                                                                 </div>
                                                             </div>
                                                             <div class="modal-footer">
                                                                 <div class="hstack gap-2 justify-content-end">
                                                                     <button type="button" class="btn btn-light"
-                                                                        data-bs-dismiss="modal">Fechar</button>
+                                                                        data-bs-dismiss="modal">Close</button>
                                                                     <button type="submit" class="btn btn-success"
-                                                                        id="add-btn">Actualizar Pastor</button>
+                                                                        id="add-btn">Add Customer</button>
                                                                     <!-- <button type="button" class="btn btn-success" id="edit-btn">Update</button> -->
                                                                 </div>
                                                             </div>
@@ -165,6 +169,7 @@
                                                     </div>
                                                 </div>
                                             </div>
+                                            {{-- Fim do modal para editar pastor --}}
 
                                             <!-- Modal -->
                                             <div class="modal fade zoomIn" id="deleteRecordModal" tabindex="-1"
@@ -179,7 +184,8 @@
                                                             <div class="mt-2 text-center">
                                                                 <lord-icon src="https://cdn.lordicon.com/gsqxdxog.json"
                                                                     trigger="loop" colors="primary:#f7b84b,secondary:#f06548"
-                                                                    style="width:100px;height:100px"></lord-icon>
+                                                                    style="width:100px;height:100px">
+                                                                </lord-icon>
                                                                 <div class="mt-4 pt-2 fs-15 mx-4 mx-sm-5">
                                                                     <h4>Are you Sure ?</h4>
                                                                     <p class="text-muted mx-4 mb-0">Are you Sure You want to
@@ -190,7 +196,8 @@
                                                                 <button type="button" class="btn w-sm btn-light"
                                                                     data-bs-dismiss="modal">Close</button>
                                                                 <button type="button" class="btn w-sm btn-danger "
-                                                                    id="delete-record">Yes, Delete It!</button>
+                                                                    id="delete-record">Yes,
+                                                                    Delete It!</button>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -198,7 +205,6 @@
                                             </div>
 
                                             {{-- Fim dos dois modais --}}
-
                                         @endforeach
                                     </tbody>
                                 </table>
