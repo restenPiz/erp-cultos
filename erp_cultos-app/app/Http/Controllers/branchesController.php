fd88006d8d573a3b8271c7c2bd5b389dacba048f
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-09 09:37:16 +0200

    Some updates in branchesController

--- erp_cultos-app/app/Http/Controllers/branchesController.php
+++ erp_cultos-app/app/Http/Controllers/branchesController.php
@@ -16,37 +16,44 @@ class branchesController extends Controller
 {
     public function addBranches()
     {
-        $users= User::where('name', '<>', 'admin')->orderBy('name')->get();
+        $users = User::where('name', '<>', 'admin')->orderBy('name')->get();
 
-        return view('Admin.addBranches',compact('users'));
+        return view('Admin.addBranches', compact('users'));
     }
     public function storeBranches()
     {
-        $table=new Branche();
+        $table = new Branche();
 
-        $table->Name=Request::input('Name');
-        $table->Creation_year=Request::input('Creation_year');
-        $table->Address=Request::input('Address');
-        $table->Number_of_members=Request::input('Number_of_members');
-        $table->Id_user=Request::input('Id_user');
+        $table->Name = Request::input('Name');
+        $table->Creation_year = Request::input('Creation_year');
+        $table->Address = Request::input('Address');
+        $table->Number_of_members = Request::input('Number_of_members');
+        $table->Id_user = Request::input('Id_user');
 
         //Inicio da parte em que encontro a chave estrangeira
-        $user=User::find(Request::input('Id_user'));
+        $user = User::find(Request::input('Id_user'));
 
         //Salvando os dados na base de dados
         $table->save();
         $user->save();
 
-        Alert::success('Adicionado','A igreja filha foi adicionada com sucesso.');
+        Alert::success('Adicionado', 'A igreja filha foi adicionada com sucesso.');
 
-        return redirect()->route('Admin.allBranche');        
+        return redirect()->route('Admin.allBranche');
     }
     public function allBranche()
     {
-        $branches=Branche::all();
+        if (Auth::user()->hasRole('admin')) 
+        {
+            $branches = Branche::all();
 
-        $users=User::where('name','<>','admin')->orderBy('name')->get();
+            $users = User::where('name', '<>', 'admin')->orderBy('name')->get();
 
-        return view('Admin.allBranche',compact('branches','users'));
+            return view('Admin.allBranches', compact('branches', 'users'));
+        }
+        else
+        {
+            return redirect()->route('login');
+        }
     }
 }
