657ae170501c73d67dc1aa79a8a60a361da161cf
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-09 08:58:36 +0200

    Some updates in branchesController

--- erp_cultos-app/app/Http/Controllers/branchesController.php
+++ erp_cultos-app/app/Http/Controllers/branchesController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\DB;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class branchesController extends Controller
 {
     public function addBranches()
     {
-        return view('Admin.addBranches');
+        $users= User::orderBy('Name', 'asc')->get();
+
+        return view('Admin.addBranches',compact('users'));
     }
 }
