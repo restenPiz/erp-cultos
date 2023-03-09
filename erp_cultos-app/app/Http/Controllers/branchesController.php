8b64aba0941170972197b2a0ae992df15ddd7279
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-09 10:25:27 +0200

    Some updates in branchesController

--- erp_cultos-app/app/Http/Controllers/branchesController.php
+++ erp_cultos-app/app/Http/Controllers/branchesController.php
@@ -81,11 +81,27 @@ class branchesController extends Controller
         if(Auth::user()->hasRole('admin'))
         {
             $table=Branche::findOrFail($id);
+
+            $table->Name=Request::input('Name');
+            $table->Address=Request::input('Address');
+            $table->Creation_year=Request::input('Creation_year');
+            $table->Id_user=Request::input('Id_user');
+            $table->Number_of_members=Request::input('Number_of_members');
+
+            $user=User::find(Request::input('Id_user'));
+
+            //Actualizando os dados na base de dados
+            $table->save();
+            $user->save();
+
+            Alert::sucess('Actualizado','A igreja filha foi adicionada com sucesso!');
+
+            return redirect()->back();
         }
         else
         {
             Alert::error('Nao Autenticado','Usuario nao autenticado');
-            
+
             return redirect()->route('login');
         }
     }
