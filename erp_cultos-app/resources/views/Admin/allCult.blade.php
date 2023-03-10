c783e97bd59be6b97763bd09d2c149e3159d310b
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-10 13:03:26 +0200

    Some updates in allCult View

--- erp_cultos-app/resources/views/Admin/allCult.blade.php
+++ erp_cultos-app/resources/views/Admin/allCult.blade.php
@@ -44,7 +44,7 @@
                                     <div class="accordion-item">
                                         <h2 class="accordion-header" id="accordionborderedExample1">
                                             <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                                data-bs-target="#accor_borderedExamplecollapse1" aria-expanded="true"
+                                                data-bs-target="#accor_borderedExamplecollapse1" aria-expanded="false"
                                                 aria-controls="accor_borderedExamplecollapse1">
                                                 Dados Temporarios?
                                             </button>
@@ -52,10 +52,11 @@
                                         <div id="accor_borderedExamplecollapse1" class="accordion-collapse collapse show"
                                             aria-labelledby="accordionborderedExample1" data-bs-parent="#accordionBordered">
                                             <div class="accordion-body">
-                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                                                richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-                                                dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-                                                tempor, sunt aliqua nulla assumenda shoreditch et.
+                                                <ul>
+                                                    <li>Dia: <b>{{$cult->Day_of_cult}}</b></li>
+                                                    <li>Horario: <b>{{$cult->Hour}}</b></li>
+                                                    <li>Duracao: <b>{{$cult->Duration}}</b></li>
+                                                </ul>
                                             </div>
                                         </div>
                                     </div>
@@ -70,10 +71,14 @@
                                         <div id="accor_borderedExamplecollapse2" class="accordion-collapse collapse"
                                             aria-labelledby="accordionborderedExample2" data-bs-parent="#accordionBordered">
                                             <div class="accordion-body">
-                                                Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis
-                                                leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed
-                                                consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida
-                                                magna mi a libero. Fusce vulputate eleifend sapien.
+                                                <ul>
+                                                    <li>Dirigente: <b>{{$cult->Leader}}</b></li>
+                                                    <li>Pregador: <b>{{$cult->Preacher}}</b></li>
+                                                    <li>Titulo: <b>{{$cult->Title}}</b></li>
+                                                    <li>Livro: <b>{{$cult->Book}}</b></li>
+                                                    <li>Capitulo: <b>{{$cult->Chapter}}</b></li>
+                                                    <li>Versiculo: <b>{{$cult->Verse}}</b></li>
+                                                </ul>
                                             </div>
                                         </div>
                                     </div>
@@ -88,10 +93,7 @@
                                         <div id="accor_borderedExamplecollapse3" class="accordion-collapse collapse"
                                             aria-labelledby="accordionborderedExample3" data-bs-parent="#accordionBordered">
                                             <div class="accordion-body">
-                                                Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum
-                                                primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui
-                                                quis mi consectetuer lacinia. Nam pretium turpis et arcu arcu tortor,
-                                                suscipit eget, imperdiet nec, imperdiet iaculis aliquam ultrices mauris.
+                                                {{$cult->Description}}
                                             </div>
                                         </div>
                                     </div>
