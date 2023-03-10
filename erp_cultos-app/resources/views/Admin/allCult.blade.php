80391d1e4f49c3249f2a55929d6b3acc00aaa690
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-10 19:24:23 +0200

    Some updates in allCult view

--- erp_cultos-app/resources/views/Admin/allCult.blade.php
+++ erp_cultos-app/resources/views/Admin/allCult.blade.php
@@ -31,7 +31,7 @@
                             <h4 class="card-title mb-0 flex-grow-1">{{ $cult->Title }}</h4>
                             <div class="flex-shrink-0">
                                 <div class="form-check form-switch form-switch-right form-switch-md">
-                                    <label for="accordion-bordered-showcode" class="form-label text-muted">Actions</label>
+                                    <label for="accordion-bordered-showcode" class="form-label text-muted">Accoes</label>
                                     <input class="form-check-input code-switcher" type="checkbox"
                                         id="accordion-bordered-showcode">
                                 </div>
@@ -53,9 +53,9 @@
                                             aria-labelledby="accordionborderedExample1" data-bs-parent="#accordionBordered">
                                             <div class="accordion-body">
                                                 <ul>
-                                                    <li>Dia: <b>{{$cult->Day_of_cult}}</b></li>
-                                                    <li>Horario: <b>{{$cult->Hour}}</b></li>
-                                                    <li>Duracao: <b>{{$cult->Duration}}</b></li>
+                                                    <li>Dia: <b>{{ $cult->Day_of_cult }}</b></li>
+                                                    <li>Horario: <b>{{ $cult->Hour }}</b></li>
+                                                    <li>Duracao: <b>{{ $cult->Duration }}</b></li>
                                                 </ul>
                                             </div>
                                         </div>
@@ -72,12 +72,12 @@
                                             aria-labelledby="accordionborderedExample2" data-bs-parent="#accordionBordered">
                                             <div class="accordion-body">
                                                 <ul>
-                                                    <li>Dirigente: <b>{{$cult->Leader}}</b></li>
-                                                    <li>Pregador: <b>{{$cult->Preacher}}</b></li>
-                                                    <li>Titulo: <b>{{$cult->Title}}</b></li>
-                                                    <li>Livro: <b>{{$cult->Book}}</b></li>
-                                                    <li>Capitulo: <b>{{$cult->Chapter}}</b></li>
-                                                    <li>Versiculo: <b>{{$cult->Verse}}</b></li>
+                                                    <li>Dirigente: <b>{{ $cult->Leader }}</b></li>
+                                                    <li>Pregador: <b>{{ $cult->Preacher }}</b></li>
+                                                    <li>Titulo: <b>{{ $cult->Title }}</b></li>
+                                                    <li>Livro: <b>{{ $cult->Book }}</b></li>
+                                                    <li>Capitulo: <b>{{ $cult->Chapter }}</b></li>
+                                                    <li>Versiculo: <b>{{ $cult->Verse }}</b></li>
                                                 </ul>
                                             </div>
                                         </div>
@@ -93,7 +93,7 @@
                                         <div id="accor_borderedExamplecollapse3" class="accordion-collapse collapse"
                                             aria-labelledby="accordionborderedExample3" data-bs-parent="#accordionBordered">
                                             <div class="accordion-body">
-                                                {{$cult->Description}}
+                                                {{ $cult->Description }}
                                             </div>
                                         </div>
                                     </div>
@@ -101,21 +101,107 @@
                             </div>
                             <div class="d-none code-view">
 
-                                {{--Inicio da parte contendo os butoes para editar e eliminar--}}
+                                {{-- Inicio da parte contendo os butoes para editar e eliminar --}}
 
-                                    <button class="btn btn-sm btn-success edit-item-btn"
-                                        data-bs-toggle="modal"
-                                        data-bs-target="#showModal{{ $cult->id }}">Editar</button>
-                                    <button class="btn btn-sm btn-danger remove-item-btn"
-                                        data-bs-toggle="modal"
-                                        data-bs-target="#deleteRecordModal{{ $cult->id }}">Eliminar</button>
+                                <button class="btn btn-sm btn-success edit-item-btn" data-bs-toggle="modal"
+                                    data-bs-target="#showModal{{ $cult->id }}">Editar</button>
+                                <button class="btn btn-sm btn-danger remove-item-btn" data-bs-toggle="modal"
+                                    data-bs-target="#deleteRecordModal{{ $cult->id }}">Eliminar</button>
 
-                                {{--Fim da parte contendo os butoes de editar e eliminar--}}
+                                {{-- Fim da parte contendo os butoes de editar e eliminar --}}
 
                             </div>
                         </div><!-- end card-body -->
                     </div><!-- end card -->
                 </div>
+
+                {{-- Inicio da parte contendo o modal para editar o culto --}}
+
+                <div class="modal fade" id="showModal{{ $cult->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header bg-light p-3">
+                                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                                    id="close-modal"></button>
+                            </div>
+                            <form class="tablelist-form" autocomplete="off"
+                                action="{{ route('updateShepherd', ['id' => $user->id]) }}" method="post">
+                                @csrf
+                                <div class="modal-body">
+                                    <div class="mb-3" id="modal-id">
+                                        <label for="customername-field" class="form-label">Nome</label>
+                                        <input type="text" id="id-field" name="name" class="form-control"
+                                            value="{{ $user->name }}" required />
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="customername-field" class="form-label">Apelido</label>
+                                        <input type="text" id="customername-field" class="form-control"
+                                            value="{{ $user->surname }}" name="surname" required />
+                                        <div class="invalid-feedback">Por favor escreva bem o seu
+                                            nome
+                                        </div>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="email-field" class="form-label">Email</label>
+                                        <input type="email" id="email-field" class="form-control"
+                                            value="{{ $user->email }}" name="email" required />
+                                        <div class="invalid-feedback">Digite o seu email de forma
+                                            correcta.</div>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="phone-field" class="form-label">Contact</label>
+                                        <input type="text" id="phone-field" class="form-control"
+                                            value="{{ $user->contact }}" name="contact" required />
+                                        <div class="invalid-feedback">Please enter a phone.</div>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="date-field" class="form-label">Funcao</label>
+                                        <input type="text" id="date-field" name="function" class="form-control"
+                                            value="{{ $user->function }}" required />
+                                        <div class="invalid-feedback">Digite a sua funcao de forma
+                                            correcta.</div>
+                                    </div>
+
+                                    <div>
+                                        <label for="status-field" class="form-label">Nivel
+                                            Teologico</label>
+                                        <select class="form-control" name="theological_level" id="status-field" required>
+                                            <option value="{{ $user->theological_level }}">
+                                                {{ $user->theological_level }}</option>
+                                            <option value="Caminho de Deus">Caminho de Deus
+                                            </option>
+                                            <option value="Caminho de Joao">Caminho de Joao
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <div class="hstack gap-2 justify-content-end">
+                                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Fechar</button>
+                                        <button type="submit" name="submit" class="btn btn-success"
+                                            id="add-btn">Actualizar
+                                            Pastor</button>
+                                        <!-- <button type="button" class="btn btn-success" id="edit-btn">Update</button> -->
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Fim da parte contendo o modal para editar o culto --}}
+
+                {{-- Inicio do modal da parte de eliminar o culto --}}
+
+
+
+                {{-- Fim do modal da parte de eliminar o culto --}}
             @endforeach
 
         </div><!-- end card -->
