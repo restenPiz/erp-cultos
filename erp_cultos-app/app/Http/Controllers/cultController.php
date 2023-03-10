9257500fa57e97323beeea2b13afe54b93e882d8
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-10 11:23:13 +0200

    Some updates in cultController

--- erp_cultos-app/app/Http/Controllers/cultController.php
+++ erp_cultos-app/app/Http/Controllers/cultController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class cultController extends Controller
 {
-    //
+    public function addCult()
+    {
+        if(Auth::user()->hasRole('admin'))
+        {
+            
+        }
+        else
+        {
+            Alert::error('Nao Autenticado','O usuario nao esta autenticado no sistem!');
+            
+            return redirect()->route('login');
+        }
+    }
 }
