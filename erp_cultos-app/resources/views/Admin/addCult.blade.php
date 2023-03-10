c923a5e3921083d7364969a92af04f0be569d38d
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-10 12:00:42 +0200

    Some updates in addCult view

--- erp_cultos-app/resources/views/Admin/addCult.blade.php
+++ erp_cultos-app/resources/views/Admin/addCult.blade.php
@@ -87,6 +87,48 @@
                 </div>
             </div>
 
+            
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header align-items-center d-flex">
+                            <h4 class="card-title mb-0 flex-grow-1">Dados do Culto</h4>
+                        </div><!-- end card header -->
+                        <div class="card-body">
+                            <div class="live-preview">
+                                <div class="row gy-4">
+                                    <div class="col-xxl-6 col-md-6">
+                                        <div>
+                                            <label for="basiInput" class="form-label">Dirigente</label>
+                                            <input type="text" class="form-control" id="basiInput"
+                                                placeholder="Digite o nome do Dirigente do culto" name="Leader">
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-6 col-md-6">
+                                        <div>
+                                            <label for="basiInput" class="form-label">Pregador</label>
+                                            <input type="text" class="form-control" id="basiInput"
+                                                placeholder="Digite o nome do Pregador" name="Preacher">
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-xxl-6 col-md-6">
+                                        <div>
+                                            <label for="labelInput" class="form-label">Tema do Culto</label>
+                                            <input type="text" class="form-control" id="labelInput"
+                                                placeholder="Digite o Tema do Culto" name="Title">
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                </div>
+                                <!--end row-->
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div class="row">
                 <div class="col-lg-2">
                     <div class="card">
