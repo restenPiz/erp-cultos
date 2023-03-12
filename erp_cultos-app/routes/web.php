a4479201ed3b30db4ae85c8978d8a9a71f6feb0a
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-12 21:07:11 +0200

    Some updates in route(Web)

--- erp_cultos-app/routes/web.php
+++ erp_cultos-app/routes/web.php
@@ -59,9 +59,9 @@ Route::group(['prefix' => 'admin', 'middleware' => ['role:admin']], function () 
     //Inicios da parte de actividade
     Route::get('/addActivity', [activityController::class, 'addActivity'])->middleware(['auth'])->name('addActivity');
     Route::post('/storeActivity', [activityController::class, 'storeActivity'])->middleware(['auth'])->name('storeActivity');
-    Route::get('/allActivity', [activityController::class, 'allActivity'])->middleware(['auth'])->name('allCult');
-    Route::post('/updateActivity/{id}', [activityController::class, 'updateActivity'])->middleware(['auth'])->name('updateCult');
-    Route::get('/deleteActivity/{id}', [activityController::class, 'deleteActivity'])->middleware(['auth'])->name('deleteCult');
+    Route::get('/allActivity', [activityController::class, 'allActivity'])->middleware(['auth'])->name('allActivity');
+    Route::post('/updateActivity/{id}', [activityController::class, 'updateActivity'])->middleware(['auth'])->name('updateActivity');
+    Route::get('/deleteActivity/{id}', [activityController::class, 'deleteActivity'])->middleware(['auth'])->name('deleteActivity');
     
 });
 
