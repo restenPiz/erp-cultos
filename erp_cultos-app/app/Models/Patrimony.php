2f68406d071861e265fa9a1afb655bc542af4944
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-15 16:27:21 +0200

    Some updates in Patrimony(Model)

--- erp_cultos-app/app/Models/Patrimony.php
+++ erp_cultos-app/app/Models/Patrimony.php
@@ -10,4 +10,10 @@ class Patrimony extends Model
     use HasFactory;
 
     protected $table='patrimonies';
+
+    protected $fillable=[
+        'Name',
+        'Quantity',
+        'Status',
+    ];
 }
