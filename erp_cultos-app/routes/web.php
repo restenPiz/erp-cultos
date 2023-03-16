0494ec1e62c23280410a409d1a94d41183fe3242
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-16 19:42:35 +0200

    Some updates in webRoutes

--- erp_cultos-app/routes/web.php
+++ erp_cultos-app/routes/web.php
@@ -65,8 +65,8 @@ Route::group(['prefix' => 'admin', 'middleware' => ['role:admin']], function () 
     Route::get('/deleteActivity/{id}', [activityController::class, 'deleteActivity'])->middleware(['auth'])->name('deleteActivity');
     
     //Inicio da parte de Patrimonio 
-    Route::get('/addPatrimony', [patrimonyController::class, 'addPatrimony'])->middleware(['auth'])->name('addActivity');
-    Route::post('/storePatrimony', [patrimonyController::class, 'storePatrimony'])->middleware(['auth'])->name('storeActivity');
+    Route::get('/addPatrimony', [patrimonyController::class, 'addPatrimony'])->middleware(['auth'])->name('addPatrimony');
+    Route::post('/storePatrimony', [patrimonyController::class, 'storePatrimony'])->middleware(['auth'])->name('storePatrimony');
     Route::get('/allPatrimony', [patrimonyController::class, 'allPatrimony'])->middleware(['auth'])->name('allActivity');
     Route::post('/updatePatrimony/{id}', [patrimonyController::class, 'updateActivity'])->middleware(['auth'])->name('updateActivity');
     Route::get('/deletePatrimony/{id}', [patrimonyController::class, 'deleteActivity'])->middleware(['auth'])->name('deleteActivity');
