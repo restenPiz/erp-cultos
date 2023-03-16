937923351329323f3dea21a1263235029b36e71c
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-16 19:53:32 +0200

    Some updates in patrimonyController

--- erp_cultos-app/app/Http/Controllers/patrimonyController.php
+++ erp_cultos-app/app/Http/Controllers/patrimonyController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class patrimonyController extends Controller
 {
-    //
+    public function addPatrimony()
+    {
+        return view('Admin.addPatrimony');
+    }
 }
