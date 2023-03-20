9ac984d4a8af28aee080330e8254df5f17eae242
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-20 12:56:20 +0200

    Some updates in addPatrimony(View)

--- erp_cultos-app/resources/views/Admin/addPatrimony.blade.php
+++ erp_cultos-app/resources/views/Admin/addPatrimony.blade.php
@@ -33,99 +33,11 @@
                                         <div>
                                             <label for="labelInput" class="form-label">Situacao</label>
                                             <select class="form-control" name="Status">
+                                                <option>...</option>
                                                 <option value="Danificado">Danificado</option>
                                                 <option value="Em Boas Condicoes">Em Boas Condicoes</option>
                                                 <option value="Em condicoes Razoaveis">Em condicoes Razoaveis</option>
                                             </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--end row-->
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-header align-items-center d-flex">
-                            <h4 class="card-title mb-0 flex-grow-1">Dados do Culto</h4>
-                        </div><!-- end card header -->
-                        <div class="card-body">
-                            <div class="live-preview">
-                                <div class="row gy-4">
-                                    <div class="col-xxl-6 col-md-6">
-                                        <div>
-                                            <label for="basiInput" class="form-label">Dirigente</label>
-                                            <input type="text" class="form-control" id="basiInput"
-                                                placeholder="Digite o nome do Dirigente do culto" name="Leader">
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-6 col-md-6">
-                                        <div>
-                                            <label for="basiInput" class="form-label">Pregador</label>
-                                            <input type="text" class="form-control" id="basiInput"
-                                                placeholder="Digite o nome do Pregador" name="Preacher">
-                                        </div>
-                                    </div>
-                                    <!--end col-->
-                                    <div class="col-xxl-6 col-md-6">
-                                        <div>
-                                            <label for="labelInput" class="form-label">Tema do Culto</label>
-                                            <input type="text" class="form-control" id="labelInput"
-                                                placeholder="Digite o Tema do Culto" name="Title">
-                                        </div>
-                                    </div>
-                                    <!--end col--><div class="col-xxl-6 col-md-6">
-                                        <div>
-                                            <label for="basiInput" class="form-label">Livro</label>
-                                            <input type="text" class="form-control" id="basiInput"
-                                                placeholder="Digite o nome do Livro" name="Book">
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-6 col-md-6">
-                                        <div>
-                                            <label for="basiInput" class="form-label">Capitulo</label>
-                                            <input type="text" class="form-control" id="basiInput"
-                                                placeholder="Digite o capitulo" name="Chapter">
-                                        </div>
-                                    </div>
-                                    <!--end col-->
-                                    <div class="col-xxl-6 col-md-6">
-                                        <div>
-                                            <label for="labelInput" class="form-label">Versiculo</label>
-                                            <input type="text" class="form-control" id="labelInput"
-                                                placeholder="Digite o Versiculo" name="Verse">
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--end row-->
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-header align-items-center d-flex">
-                            <h4 class="card-title mb-0 flex-grow-1">Dados Adicionais</h4>
-                        </div><!-- end card header -->
-                        <div class="card-body">
-                            <div class="live-preview">
-                                <div class="row gy-4">
-                                    <div class="col-xxl-12 col-md-12">
-                                        <div>
-                                            <label for="labelInput" class="form-label">Descricao</label>
-                                            <textarea type="text" class="form-control" id="labelInput"
-                                                placeholder="Digite algumas informacoes sobre o decorrer do culto" name="Description"></textarea>
                                         </div>
                                     </div>
                                 </div>
