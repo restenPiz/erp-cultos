c40d9dd5e81a8414c91dbee3639f718e5e24306c
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-21 19:45:16 +0200

    Some updates in system

--- erp_cultos-app/app/Http/Controllers/dashboardController.php
+++ erp_cultos-app/app/Http/Controllers/dashboardController.php
@@ -7,18 +7,15 @@ use Illuminate\Support\Facades\Auth;
 
 class dashboardController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware(['role:admin']);
-    }
     public function index()
     {
         if(Auth::user()->hasRole('admin'))
         {
             return view('Admin.Index');
-        }else{
-            return 404;
+        }
+        if(Auth::user()->hasRole('shepherd'))
+        {
+            return view('Shepherd.Index');
         }
     }
 }
--- erp_cultos-app/resources/views/Shepherd/Index.blade.php
+++ erp_cultos-app/resources/views/Shepherd/Index.blade.php
@@ -1,1 +1,22 @@
-<h1>Bem vindo Pastor</h1>
+@extends('Layout.Index')
+@section('content')
+     <!-- start page title -->
+     <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                <h4 class="mb-sm-0">Erp-Cultos</h4>
+
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Paginas</a>
+                        </li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- end page title -->
+
+@endsection
