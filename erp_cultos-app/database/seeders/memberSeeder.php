1f25a63dfa961ce57771fac58458eedf086bb9e8
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-22 12:55:39 +0200

    Some updates in memberSeeder

--- erp_cultos-app/database/seeders/memberSeeder.php
+++ erp_cultos-app/database/seeders/memberSeeder.php
@@ -14,7 +14,21 @@ class memberSeeder extends Seeder
     public function run()
     {
         Member::create([
-            
+            'Name'=>'',
+            'Password'=>'',
+            'Password_confirmation'=>'',
+            'Email'=>'',
+            'Profession'=>'',
+            'Number_of_bi'=>'',
+            'Gender'=>'',
+            'Contact'=>'',
+            'Surname'=>'',
+            'Date_of_birth'=>'',
+            'Marital_status'=>'',
+            'Baptism'=>'',
+            'Time_in_church'=>'',
+            'Affiliation'=>'',
+            'Theological_level'=>'',
         ]);
     }
 }
