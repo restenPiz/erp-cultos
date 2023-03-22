947d659a0db15e09846ead320ff38e4248c158c8
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-22 13:02:35 +0200

    Some updates in memberSeeder

--- erp_cultos-app/database/seeders/memberSeeder.php
+++ erp_cultos-app/database/seeders/memberSeeder.php
@@ -4,31 +4,33 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Member;
+use Illuminate\Support\Facades\DB;
 class memberSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        Member::create([
-            'Name'=>'',
-            'Password'=>'',
-            'Password_confirmation'=>'',
-            'Email'=>'',
-            'Profession'=>'',
-            'Number_of_bi'=>'',
-            'Gender'=>'',
-            'Contact'=>'',
-            'Surname'=>'',
-            'Date_of_birth'=>'',
-            'Marital_status'=>'',
-            'Baptism'=>'',
-            'Time_in_church'=>'',
-            'Affiliation'=>'',
-            'Theological_level'=>'',
-        ]);
+        $dados = [];
+
+        for ($i = 0; $i < 1000; $i++) {
+            $dados[] = [
+                'Name'=>'Mauro',
+                'Password'=>'mauro123',
+                'Password_confirmation'=>'mauro123',
+                'Email'=>'mauropeniel@gmail.com',
+                'Profession'=>'Software Engineer',
+                'Number_of_bi'=>'0092983729398AB',
+                'Gender'=>'Masculino',
+                'Contact'=>'867336817',
+                'Surname'=>'Peniel',
+                'Date_of_birth'=>'11/09/2002',
+                'Marital_status'=>'Solteiro',
+                'Baptism'=>'Nao baptizado',
+                'Time_in_church'=>'2 anos',
+                'Affiliation'=>'Peniel e Assuncao',
+                'Theological_level'=>'Caminho de Deus',            
+            ];
+        }
+
+        DB::table('departments')->insert($dados);
     }
 }
