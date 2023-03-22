d4264f94e465ce1e735fde639bf21ce21640588d
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-22 12:35:36 +0200

    Some updates in department_member (Model)

--- erp_cultos-app/app/Models/department_member.php
+++ erp_cultos-app/app/Models/department_member.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class department_member extends Model
+class department_member extends Pivot
 {
     use HasFactory;
+
+    protected $table='department_members';
+
+    protected $fillable=[
+        'id_member',
+        'id_department'
+    ];
 }
