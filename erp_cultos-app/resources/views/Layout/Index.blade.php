3530b56cc350c4dbbf1e00efed1e75a49eab18a8
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-23 23:59:42 +0200

    Some updates in Layout(Index)

--- erp_cultos-app/resources/views/Layout/Index.blade.php
+++ erp_cultos-app/resources/views/Layout/Index.blade.php
@@ -981,6 +981,35 @@
                         </button>
                     </div>
                 @endrole
+                
+                @role('shepherd')
+                <div class="app-menu navbar-menu">
+                    <!-- LOGO -->
+                    <div class="navbar-brand-box">
+                        <!-- Dark Logo-->
+                        <a href="index.html" class="logo logo-dark">
+                            <span class="logo-sm">
+                                <img src="assets/images/a1.png" alt="" height="22">
+                            </span>
+                            <span class="logo-lg">
+                                <img src="assets/images/a1.png" alt="" height="17">
+                            </span>
+                        </a>
+                        <!-- Light Logo-->
+                        <br><a href="{{ route('dashboard') }}" class="logo logo-light">
+                            <span class="logo-sm">
+                                <img src="../assets/images/a1.png" alt="" height="30">
+                            </span>
+                            <span class="logo-lg">
+                                <img src="../assets/images/a1.png" alt="" height="30">
+                            </span>
+                        </a>
+                        <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
+                            id="vertical-hover">
+                            <i class="ri-record-circle-line"></i>
+                        </button>
+                    </div>
+                @endrole
 
                 <div id="scrollbar">
                     <div class="container-fluid">
