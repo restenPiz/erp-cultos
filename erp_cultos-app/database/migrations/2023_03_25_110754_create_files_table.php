8cc067a184f766a605721039bbdfd378ebc35a2b
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-25 13:27:32 +0200

    Some updates in system

--- erp_cultos-app/database/migrations/2023_03_25_110754_create_files_table.php
+++ erp_cultos-app/database/migrations/2023_03_25_110754_create_files_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateFilesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('files', function (Blueprint $table) {
@@ -23,11 +18,6 @@ class CreateFilesTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('files');
