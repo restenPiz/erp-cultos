bf76b7e6bdbf3414c55a7b5906e6e48a24fa94d0
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-25 12:56:41 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Worship_leader/Index.blade.php
+++ erp_cultos-app/resources/views/Worship_leader/Index.blade.php
@@ -36,32 +36,8 @@
                             <div class="p-2">
                                 <h3 class="text-white mb-1">{{Auth::user()->name}}</h3>
                                 <p class="text-white-75">{{Auth::user()->userType}}</p>
-                                <div class="hstack text-white-50 gap-1">
-                                    <div class="me-2"><i class="ri-map-pin-user-line me-1 text-white-75 fs-16 align-middle"></i>California, United States</div>
-                                    <div>
-                                        <i class="ri-building-line me-1 text-white-75 fs-16 align-middle"></i>Themesbrand
-                                    </div>
-                                </div>
                             </div>
                         </div>
-                        <!--end col-->
-                        <div class="col-12 col-lg-auto order-last order-lg-0">
-                            <div class="row text text-white-50 text-center">
-                                <div class="col-lg-6 col-4">
-                                    <div class="p-2">
-                                        <h4 class="text-white mb-1">24.3K</h4>
-                                        <p class="fs-14 mb-0">Followers</p>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 col-4">
-                                    <div class="p-2">
-                                        <h4 class="text-white mb-1">1.3K</h4>
-                                        <p class="fs-14 mb-0">Following</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!--end col-->
 
                     </div>
                     <!--end row-->
