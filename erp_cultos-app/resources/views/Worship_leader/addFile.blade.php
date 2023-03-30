77b9e29daf15944914d1e42f2b20ba47e93eeeb0
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-31 00:07:38 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Worship_leader/addFile.blade.php
+++ erp_cultos-app/resources/views/Worship_leader/addFile.blade.php
@@ -51,22 +51,7 @@
                         <ul class="nav nav-tabs-custom rounded card-header-tabs border-bottom-0" role="tablist">
                             <li class="nav-item">
                                 <a class="nav-link active" data-bs-toggle="tab" href="#personalDetails" role="tab">
-                                    <i class="fas fa-home"></i> Personal Details
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" data-bs-toggle="tab" href="#changePassword" role="tab">
-                                    <i class="far fa-user"></i> Change Password
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" data-bs-toggle="tab" href="#experience" role="tab">
-                                    <i class="far fa-envelope"></i> Experience
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" data-bs-toggle="tab" href="#privacy" role="tab">
-                                    <i class="far fa-envelope"></i> Privacy Policy
+                                    <i class="fas fa-home"></i> Inserir Ficheiro
                                 </a>
                             </li>
                         </ul>
