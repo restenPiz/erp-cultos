bc2f0b7f690f78a0ae3ea5bbc47bc9d4f936e24a
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-04 00:14:15 +0200

    Some updates in system

--- erp_cultos-app/app/Http/Controllers/fileController.php
+++ erp_cultos-app/app/Http/Controllers/fileController.php
@@ -68,6 +68,7 @@ class fileController extends Controller
             $documents = DB::table('files')->where('Type_file', 'Arquivo_arquivo')->get();
             $document_text = DB::table('files')->where('Type_file', 'Arquivo_texto')->get();
             $images = DB::table('files')->where('Type_file', 'Arquivo_imagem')->get();
+            $videos = DB::table('files')->where('Type_file', 'Arquivo_video')->get();
 
             return view('Worship_leader.Index', compact('files'));
         }
