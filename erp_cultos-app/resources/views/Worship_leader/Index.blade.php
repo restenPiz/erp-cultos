de9281633b32c589e0bbc510990396f7a97205db
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-04 11:13:07 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Worship_leader/Index.blade.php
+++ erp_cultos-app/resources/views/Worship_leader/Index.blade.php
@@ -1776,7 +1776,8 @@
                                                                             <div class="fallback">
                                                                                 <input name="File" type="file"
                                                                                     multiple="multiple"
-                                                                                    class="form-control" value="{{$image->File}}">
+                                                                                    class="form-control"
+                                                                                    value="{{ $image->File }}">
                                                                             </div>
                                                                             <div class="dz-message needsclick"
                                                                                 style="text-align: center">
@@ -1806,6 +1807,58 @@
                                                     </div>
                                                 </div>
                                                 {{-- Fim do modal para editar Ficheiros --}}
+
+                                                {{-- Inicio do modal para eliminar os ficheiros --}}
+
+
+                                                <!-- Modal -->
+                                                <div class="modal fade zoomIn"
+                                                    id="deleteRecordModal{{ $image->id }}" tabindex="-1"
+                                                    aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-centered">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <button type="button" class="btn-close"
+                                                                    data-bs-dismiss="modal" aria-label="Close"
+                                                                    id="btn-close"></button>
+                                                            </div>
+                                                            <form
+                                                                action="{{ route('deleteFile', ['id' => $image->id]) }}"
+                                                                method="get">
+                                                                @csrf
+                                                                @method('DELETE')
+                                                                <div class="modal-body">
+                                                                    <div class="mt-2 text-center">
+                                                                        <lord-icon
+                                                                            src="https://cdn.lordicon.com/gsqxdxog.json"
+                                                                            trigger="loop"
+                                                                            colors="primary:#f7b84b,secondary:#f06548"
+                                                                            style="width:100px;height:100px">
+                                                                        </lord-icon>
+                                                                        <div class="mt-4 pt-2 fs-15 mx-4 mx-sm-5">
+                                                                            <h4>Voce tem certeza ?</h4>
+                                                                            <p class="text-muted mx-4 mb-0">Voce pretende
+                                                                                eliminar
+                                                                                {{ $image->Name_file }} ?</p>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div
+                                                                        class="d-flex gap-2 justify-content-center mt-4 mb-2">
+                                                                        <button type="button"
+                                                                            class="btn w-sm btn-light"
+                                                                            data-bs-dismiss="modal">Fechar</button>
+                                                                        <button type="submit" name="submit"
+                                                                            class="btn w-sm btn-danger "
+                                                                            id="delete-record">Sim,
+                                                                            elimine!</button>
+                                                                    </div>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                {{-- Fim do modal para eliminar os ficheiros --}}
 
                                             </div>
                                         @endforeach
