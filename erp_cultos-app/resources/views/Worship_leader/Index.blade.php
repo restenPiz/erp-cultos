9a8690172833eb1be6e5445c9f59b9f430a8b259
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-04 10:20:59 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Worship_leader/Index.blade.php
+++ erp_cultos-app/resources/views/Worship_leader/Index.blade.php
@@ -1332,6 +1332,7 @@
                                             </div>---}}
                         
                                             <div class="row row-cols-xl-5 row-cols-lg-3 row-cols-md-2 row-cols-1">
+                                                {{--Inicio das imagens do sistema--}}
                                                 <div class="col">
                                                     <div class="card explore-box card-animate">
                                                         <div class="bookmark-icon position-absolute top-0 end-0 p-2">
@@ -1341,8 +1342,10 @@
                                                             <img src="../assets/images/nft/img-02.jpg" alt="" class="card-img-top explore-img" />
                                                             <div class="bg-overlay"></div>
                                                             <div class="place-bid-btn">
-                                                                <a href="#!" class="btn btn-success edit-item-btn"> Editar</a>
-                                                                <a href="#!" class="btn btn-danger remove-item-btn"> Eliminar</a>
+                                                                <div class="col-lg-12">
+                                                                    <a href="#!" class="btn btn-success edit-item-btn"> Editar</a>
+                                                                    <a href="#!" class="btn btn-danger remove-item-btn"> Eliminar</a>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                         <div class="card-body">
@@ -1360,114 +1363,7 @@
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div class="col">
-                                                    <div class="card explore-box card-animate">
-                                                        <div class="bookmark-icon position-absolute top-0 end-0 p-2">
-                                                            <button type="button" class="btn btn-icon active" data-bs-toggle="button" aria-pressed="true"><i class="mdi mdi-cards-heart fs-16"></i></button>
-                                                        </div>
-                                                        <div class="explore-place-bid-img">
-                                                            <img src="../../../../../img.themesbrand.com/velzon/images/img-2.gif" alt="" class="card-img-top explore-img" />
-                                                            <div class="bg-overlay"></div>
-                                                            <div class="place-bid-btn">
-                                                                <a href="#!" class="btn btn-success"><i class="ri-auction-fill align-bottom me-1"></i> Place Bid</a>
-                                                            </div>
-                                                        </div>
-                                                        <div class="card-body">
-                                                            <p class="fw-medium mb-0 float-end"><i class="mdi mdi-heart text-danger align-middle"></i> 94.1k </p>
-                                                            <h5 class="mb-1"><a href="apps-nft-item-details.html">Trendy Fashion Portraits</a></h5>
-                                                            <p class="text-muted mb-0">3d Style</p>
-                                                        </div>
-                                                        <div class="card-footer border-top border-top-dashed">
-                                                            <div class="d-flex align-items-center">
-                                                                <div class="flex-grow-1 fs-14">
-                                                                    <i class="ri-price-tag-3-fill text-warning align-bottom me-1"></i> Highest: <span class="fw-medium">674.92 ETH</span>
-                                                                </div>
-                                                                <h5 class="flex-shrink-0 fs-14 text-primary mb-0">563.81 ETH</h5>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="card explore-box card-animate">
-                                                        <div class="bookmark-icon position-absolute top-0 end-0 p-2">
-                                                            <button type="button" class="btn btn-icon active" data-bs-toggle="button" aria-pressed="true"><i class="mdi mdi-cards-heart fs-16"></i></button>
-                                                        </div>
-                                                        <div class="explore-place-bid-img">
-                                                            <img src="../assets/images/nft/img-04.jpg" alt="" class="card-img-top explore-img" />
-                                                            <div class="bg-overlay"></div>
-                                                            <div class="place-bid-btn">
-                                                                <a href="#!" class="btn btn-success"><i class="ri-auction-fill align-bottom me-1"></i> Place Bid</a>
-                                                            </div>
-                                                        </div>
-                                                        <div class="card-body">
-                                                            <p class="fw-medium mb-0 float-end"><i class="mdi mdi-heart text-danger align-middle"></i> 34.12k </p>
-                                                            <h5 class="mb-1"><a href="apps-nft-item-details.html">Smillevers Crypto</a></h5>
-                                                            <p class="text-muted mb-0">Crypto Card</p>
-                                                        </div>
-                                                        <div class="card-footer border-top border-top-dashed">
-                                                            <div class="d-flex align-items-center">
-                                                                <div class="flex-grow-1 fs-14">
-                                                                    <i class="ri-price-tag-3-fill text-warning align-bottom me-1"></i> Highest: <span class="fw-medium">41.658 ETH</span>
-                                                                </div>
-                                                                <h5 class="flex-shrink-0 fs-14 text-primary mb-0">34.81 ETH</h5>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="card explore-box card-animate">
-                                                        <div class="bookmark-icon position-absolute top-0 end-0 p-2">
-                                                            <button type="button" class="btn btn-icon active" data-bs-toggle="button" aria-pressed="true"><i class="mdi mdi-cards-heart fs-16"></i></button>
-                                                        </div>
-                                                        <div class="explore-place-bid-img">
-                                                            <img src="../../../../../img.themesbrand.com/velzon/images/img-4.gif" alt="" class="card-img-top explore-img" />
-                                                            <div class="bg-overlay"></div>
-                                                            <div class="place-bid-btn">
-                                                                <a href="#!" class="btn btn-success"><i class="ri-auction-fill align-bottom me-1"></i> Place Bid</a>
-                                                            </div>
-                                                        </div>
-                                                        <div class="card-body">
-                                                            <p class="fw-medium mb-0 float-end"><i class="mdi mdi-heart text-danger align-middle"></i> 15.93k </p>
-                                                            <h5 class="mb-1"><a href="apps-nft-item-details.html">Evolved Reality</a></h5>
-                                                            <p class="text-muted mb-0">Video</p>
-                                                        </div>
-                                                        <div class="card-footer border-top border-top-dashed">
-                                                            <div class="d-flex align-items-center">
-                                                                <div class="flex-grow-1 fs-14">
-                                                                    <i class="ri-price-tag-3-fill text-warning align-bottom me-1"></i> Highest: <span class="fw-medium">2.75ETH</span>
-                                                                </div>
-                                                                <h5 class="flex-shrink-0 fs-14 text-primary mb-0">3.167 ETH</h5>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="card explore-box card-animate">
-                                                        <div class="bookmark-icon position-absolute top-0 end-0 p-2">
-                                                            <button type="button" class="btn btn-icon active" data-bs-toggle="button" aria-pressed="true"><i class="mdi mdi-cards-heart fs-16"></i></button>
-                                                        </div>
-                                                        <div class="explore-place-bid-img">
-                                                            <img src="../assets/images/nft/img-01.jpg" alt="" class="card-img-top explore-img" />
-                                                            <div class="bg-overlay"></div>
-                                                            <div class="place-bid-btn">
-                                                                <a href="#!" class="btn btn-success"><i class="ri-auction-fill align-bottom me-1"></i> Place Bid</a>
-                                                            </div>
-                                                        </div>
-                                                        <div class="card-body">
-                                                            <p class="fw-medium mb-0 float-end"><i class="mdi mdi-heart text-danger align-middle"></i> 14.85k </p>
-                                                            <h5 class="mb-1"><a href="apps-nft-item-details.html">Abstract Face Painting</a></h5>
-                                                            <p class="text-muted mb-0">Collectibles</p>
-                                                        </div>
-                                                        <div class="card-footer border-top border-top-dashed">
-                                                            <div class="d-flex align-items-center">
-                                                                <div class="flex-grow-1 fs-14">
-                                                                    <i class="ri-price-tag-3-fill text-warning align-bottom me-1"></i> Highest: <span class="fw-medium">122.34ETH</span>
-                                                                </div>
-                                                                <h5 class="flex-shrink-0 fs-14 text-primary mb-0">97.8 ETH</h5>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
+                                                {{--Fim das imagens do sistema--}}
                                             </div>
                         <!--end row-->
                                         </div>
