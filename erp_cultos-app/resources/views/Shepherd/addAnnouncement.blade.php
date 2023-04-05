0a02147f1bc45c9e9cee69ae8e8247f00be9ddc5
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-05 17:00:25 +0200

    Some updates in system

---
+++ erp_cultos-app/resources/views/Shepherd/addAnnouncement.blade.php
@@ -0,0 +1,12 @@
+@extends('Layout.Index')
+
+@section('content')
+    @role('admin')
+
+    {{--Inicio do conteudo da parte de comunicado--}}
+
+    {{--Fim do conteudo da parte de comunicado--}}
+
+    @endrole
+
+@endsection
