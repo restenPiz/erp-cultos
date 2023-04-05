5c6a6b744cffe668d1a064ef311cb1c1b7188c0d
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-05 11:25:39 +0200

    Some updates in system

--- erp_cultos-app/app/Http/Controllers/dashboardController.php
+++ erp_cultos-app/app/Http/Controllers/dashboardController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Support\Facades\File;
 
 class dashboardController extends Controller
 {
@@ -19,7 +22,11 @@ class dashboardController extends Controller
         }
         if(Auth::user()->hasRole('worship_leader'))
         {
-            return redirect()->route('allFile');
+            $files = DB::table('files')->where('Type_file', 'Arquivo_arquivo')->get();
+            $images = DB::table('files')->where('Type_file', 'Arquivo_imagem')->get();
+            $videos = DB::table('files')->where('Type_file', 'Arquivo_video')->get();
+
+            return view('Worship_leader.Index', compact('files','images','videos'));
         }
     }
 }
