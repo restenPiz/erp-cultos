930e53e200d564eb5dba57505f28e9092c0caa48
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-05 16:36:49 +0200

    Some updates in system

--- erp_cultos-app/database/migrations/2023_04_05_141158_create_announcements_table.php
+++ erp_cultos-app/database/migrations/2023_04_05_141158_create_announcements_table.php
@@ -15,6 +15,14 @@ class CreateAnnouncementsTable extends Migration
     {
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
+            $table->string('Type');
+            $table->string('Description');
+            $table->string('Hour');
+
+            //Inicio da coluna contendo a chave estrangeiraa
+            $table->integer('Id_user')->unsigned();
+            $table->foreign('Id_user')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
