c06b7a2a15e44c0272d62cda2a44b2a2668ef84f
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-05 16:47:22 +0200

    Some updates in system

--- erp_cultos-app/app/Http/Controllers/announcementController.php
+++ erp_cultos-app/app/Http/Controllers/announcementController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use Request;
+use App\Models\User;
+use App\Models\Announcement;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\DB;
+use RealRashid\SweetAlert\Facades\Alert;
 class announcementController extends Controller
 {
-    //
+    public function addAnnouncement()
+    {
+        if(Auth::user()->hasRole('shepherd'))
+        {
+
+        }else{
+
+            Alert::error('Nao Autenticado!','O usuario nao esta autenticado no sistema!');
+
+            return redirect()->route('login');
+        }
+    }    
+
+    public function storeAnnouncement()
+    {
+        if(Auth::user()->hasRole('shepherd'))
+        {
+
+        }else{
+
+            Alert::error('Nao Autenticado!','O usuario nao esta autenticado no sistema!');
+
+            return redirect()->route('login');
+        }
+    }
+    public function updateAnnouncement($id)
+    {
+        if(Auth::user()->hasRole('shepherd'))
+        {
+
+        }else{
+
+            Alert::error('Nao Autenticado!','O usuario nao esta autenticado no sistema!');
+            
+            return redirect()->route('login');
+        }
+    }
+    public function deleteAnnouncement($id)
+    {
+        if(Auth::user()->hasRole('shepherd'))
+        {
+
+        }else{
+
+            Alert::error('Nao Autenticado!','O usuario nao esta autenticado no sistema!');
+
+            return redirect()->route('login');
+        }
+    }
 }
