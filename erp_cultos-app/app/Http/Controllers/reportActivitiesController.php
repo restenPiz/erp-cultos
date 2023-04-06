bf8e3d40a4e472a62aa26a26e0e94e2a558320f7
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-06 14:30:28 +0200

    Some updates in system

--- erp_cultos-app/app/Http/Controllers/reportActivitiesController.php
+++ erp_cultos-app/app/Http/Controllers/reportActivitiesController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Request;
+use App\Models\User;
 use App\Models\Activity;
-use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\DB;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class reportActivitiesController extends Controller
 {
