8a084f45cae2f3e9e01c33a1a41fdc1d403c7dee
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-06 14:28:00 +0200

    Some updates in system(ReportActivities)

--- erp_cultos-app/app/Http/Controllers/reportActivitiesController.php
+++ erp_cultos-app/app/Http/Controllers/reportActivitiesController.php
@@ -2,9 +2,110 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
 use Illuminate\Http\Request;
 
 class reportActivitiesController extends Controller
 {
-    //
+    public function addReportActivities()
+    {
+        if(Auth::user()->hasRole('shepherd')){
+
+            return view('Shepherd.addReportActivities');
+        }
+        else{
+
+            Alert::error('Nao Autenticado!','O usuario nao esta autenticado no sistema!');
+
+            return redirect()->route('login');
+        }
+    }
+    public function allReportActivities()
+    {
+        if(Auth::user()->hasRole('shepherd')){
+
+            $reportActivities=DB::table('activities')
+                ->where('Group','=','Admin')
+                ->get();
+
+            return view('Shepherd.allReportActvities' ,compact('reportActivities'));            
+        }
+        else{
+            
+            Alert::error('Nao Autenticado!','O usuario nao esta autenticado no sistema!');
+
+            return redirect()->route('login');
+        }
+    }
+    public function storeReportActivities()
+    {
+        if(Auth::user()->hasRole('shepherd'))
+        {
+            $table=new Activity();
+
+            $table->Title=Request::input('Title');
+            $table->Hour=Request::input('Hour');
+            $table->Group=Request::input('Group');
+            $table->Id_user=Request::input('Id_user');
+    
+            $user=User::find(Request::input('Id_user'));
+    
+            $table->save();
+            $user->save();
+    
+            Alert::success('Adicionado!','O seu relatorio de actividades foi adicionado com sucesso!');
+    
+            return redirect()->back();
+        }
+        else{
+            
+            Alert::error('Nao Autenticado!','O usuario nao esta autenticado no sistema!');
+
+            return redirect()->route('login');
+        }
+    }
+    public function updateReportActivities($id)
+    {
+        if(Auth::user()->hasRole('shepherd'))
+        {
+            $table=Activity::findOrFail($id);
+
+            $table->Title=Request::input('Title');
+            $table->Hour=Request::input('Hour');
+            $table->Group=Request::input('Group');
+            $table->Id_user=Request::input('Id_user');
+
+            $user=User::find(Request::input('Id_user'));
+
+            $table->save();
+            $user->save();
+
+            Alert::success('Actualizado!','O seu relatorio de actividades foi actualizado com sucesso!');
+
+            return redirect()->back();
+        }
+        else
+        {
+        
+            Alert::error('Nao Autenticado!','O usuario nao esta autenticado no sistema!');
+
+            return redirect()->route('login');
+        }
+    }
+    public function deleteReportActivities($id)
+    {
+        if(Auth::user()->hasRole('shepherd'))
+        {
+            $table=Activity::findOrFail($id);
+            
+            $table->delete();
+
+            Alert::success('Eliminado!','O relatorio de actividades foi eliminado com sucesso!');
+        }
+        else{
+            Alert::error('Nao Autenticado!','O usuario nao esta autenticado no sistema!');
+
+            return redirect()->route('login');
+        }
+    }
 }
