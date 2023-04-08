378551481150e609a0c56858911da29e7ddde0c4
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-08 12:02:45 +0200

    Some updates in system

---
+++ erp_cultos-app/database/migrations/2023_04_08_100219_create_outputs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOutputsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('outputs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('outputs');
+    }
+}
