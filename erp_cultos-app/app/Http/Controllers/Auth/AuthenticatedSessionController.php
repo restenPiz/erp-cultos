5189851dbcc7fe683cdf327b7b29bae0407885f9
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-09 22:12:55 +0200

    Some updates in system

--- erp_cultos-app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ erp_cultos-app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -42,13 +42,14 @@ class AuthenticatedSessionController extends Controller
     }*/
     public function destroy(Request $request)
     {
-        Alert::warning('Logout Concluido',);
 
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
+        
+        Alert::warning('Logout Concluido',);
 
         return redirect('/');
     }
--- erp_cultos-app/app/Models/Input.php
+++ erp_cultos-app/app/Models/Input.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Input extends Model
 {
     use HasFactory;
+
+    protected $table='inputs';
+
+    protected $fillable=[
+        
+    ];
 }
