4fe86c0c78def2fb80db34a3670f7d8795cc9947
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-15 05:38:24 +0200

    Some updates in system

--- erp_cultos-app/app/Http/Controllers/departmentController.php
+++ erp_cultos-app/app/Http/Controllers/departmentController.php
@@ -33,7 +33,7 @@ class departmentController extends Controller
     {
         if(Auth::user()->hasRole('admin'))
         {
-            $departments=DB::table('departments')->distinct('Name','id')->pluck('Name','id');
+            $departments=Department::all();
 
             $users=DB::table('users')
                 ->where('id','<>',1)
