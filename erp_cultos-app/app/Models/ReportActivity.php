79f9ed5023cb04d16b3629c3c3a6f918009b91fe
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-18 09:41:26 +0200

    Some updates in system

--- erp_cultos-app/app/Models/ReportActivity.php
+++ erp_cultos-app/app/Models/ReportActivity.php
@@ -21,11 +21,6 @@ class ReportActivity extends Model
     //Inicio dos metodos que fazem a captura dos dados
     public function users()
     {
-        return $this->hasMany(User::class);
-    }
-    
-    public function name($id)
-    {
-        return User::find($id)->name;
+        return $this->belongsTo(User::class,'Id_user');
     }
 }
