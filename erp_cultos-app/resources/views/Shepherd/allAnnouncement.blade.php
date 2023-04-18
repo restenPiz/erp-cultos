fdfe7d3c2c58235dbd459e70008d8413a7126d92
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-18 08:16:10 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Shepherd/allAnnouncement.blade.php
+++ erp_cultos-app/resources/views/Shepherd/allAnnouncement.blade.php
@@ -50,6 +50,7 @@
                         </div>
 
                         <div class="table-responsive table-card mt-3 mb-1">
+                            @if(count($announcements)>0)
                             <table class="table align-middle table-nowrap" id="customerTable">
                                 <thead class="table-light">
                                     <tr>
@@ -135,16 +136,18 @@
                                     @endforeach
                                 </tbody>
                             </table>
-                            <div class="noresult" style="display: none">
+                            @else
+                            <div class="noresult">
                                 <div class="text-center">
                                     <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
                                         colors="primary:#121331,secondary:#08a88a" style="width:75px;height:75px">
                                     </lord-icon>
-                                    <h5 class="mt-2">Sorry! No Result Found</h5>
-                                    <p class="text-muted mb-0">We've searched more than 150+ Orders We did not find any
-                                        orders for you search.</p>
+                                    <h5 class="mt-2">Desculpe! Nenhum resultado encontrado</h5>
+                                    {{-- -<p class="text-muted mb-0">We've searched more than 150+ Orders We did not find any
+                            orders for you search.</p>- --}}
                                 </div>
                             </div>
+                            @endif
                         </div>
 
                         <div class="d-flex justify-content-end">
