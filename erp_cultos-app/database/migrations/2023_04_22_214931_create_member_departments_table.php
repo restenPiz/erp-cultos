7a09390b12d32221aa05d608f882d1e69154a097
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-23 00:17:34 +0200

    Some updates in system

--- erp_cultos-app/database/migrations/2023_04_22_214931_create_member_departments_table.php
+++ erp_cultos-app/database/migrations/2023_04_22_214931_create_member_departments_table.php
@@ -15,6 +15,16 @@ class CreateMemberDepartmentsTable extends Migration
     {
         Schema::create('member_departments', function (Blueprint $table) {
             $table->id();
+
+            //Inicio das colunas das chaves estrangeiras
+            
+            $table->integer('Id_user')->unsigned();
+            $table->foreign('Id_user')->references('id')->on('users');
+
+            
+            $table->integer('Id_departments')->unsigned();
+            $table->foreign('Id_departments')->references('id')->on('departments');
+
             $table->timestamps();
         });
     }
