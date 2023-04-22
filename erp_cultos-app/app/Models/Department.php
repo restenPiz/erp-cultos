29c6cc45956e7429e465474ef90ada414b0f95ab
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-22 23:58:10 +0200

    Some updates in system

--- erp_cultos-app/app/Models/Department.php
+++ erp_cultos-app/app/Models/Department.php
@@ -16,13 +16,9 @@ class Department extends Model
     ];
 
     //Inicio dos metodos responsaveis por interligar a tabela de departamentos com a de usuarios
-    public function users()
+    public function nivel()
     {
-        return $this->hasMany(User::class);
-    }
-    public function name($id)
-    {
-        return User::find($id)->name;
+        return $this->belongsToMany(User::class, 'Id_user','Id_department');
     }
     public function activitys()
     {
