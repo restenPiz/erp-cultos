d8e0ead4605e0b38d9604497128c0bd552e0b231
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-23 00:02:28 +0200

    Some updates in system

--- erp_cultos-app/app/Models/member_department.php
+++ erp_cultos-app/app/Models/member_department.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class member_department extends Model
 {
     use HasFactory;
+
+    protected $table='member_departments';
+
+    protected $fillable=[
+        'Id_user',
+        'Id_department'
+    ];
+
+    //Inicio da parte de relacionamento com as outras tabelas
+    public function department() {
+        return $this->belongsTo(Department::class,'Id_department');
+    }
+    public function users() {
+        return $this->belongsTo(User::class,'Id_user');
+    }
 }
