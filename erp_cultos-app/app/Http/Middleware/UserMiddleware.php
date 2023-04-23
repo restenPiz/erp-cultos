4a69deaf8b4a6661fb5f53e15fdb8673dd52af3b
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-23 02:50:31 +0200

    Some updates in system

---
+++ erp_cultos-app/app/Http/Middleware/UserMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Alert;
+
+class UserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::check() && Auth::user()->status)
+        {
+            $banned=Auth::user()->status=="1";
+            Auth::logout();
+
+            if($banned==1)
+            {
+                Alert::error('Bloqueado!','Sua conta foi bloqueada, por favor contacte o administrador');
+            }
+
+            Alert::error('Bloqueado!','Sua conta foi bloqueada, por favor contacte o administrador');
+            return redirect()->route('login');       
+        }
+    }
+}
