659584b8cbdf0aad58164cbd4c588a67bee8f0e3
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-27 09:34:55 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Member/Index.blade.php
+++ erp_cultos-app/resources/views/Member/Index.blade.php
@@ -23,7 +23,7 @@
     <div class="container-fluid">
         <div class="profile-foreground position-relative mx-n4 mt-n4">
             <div class="profile-wid-bg">
-                <img src="assets/images/profile-bg.jpg" alt="" class="profile-wid-img" />
+                <img src="" alt="" class="profile-wid-img" />
             </div>
         </div>
         <div class="pt-4 mb-4 mb-lg-3 pb-lg-4">
