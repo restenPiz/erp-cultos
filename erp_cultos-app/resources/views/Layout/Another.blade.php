d7f80467d1ef3c3d2284908051430fd37d931537
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-28 23:48:12 +0200

    Sp,e updates in sytem

--- erp_cultos-app/resources/views/Layout/Another.blade.php
+++ erp_cultos-app/resources/views/Layout/Another.blade.php
@@ -307,7 +307,7 @@
                             </div>
                         </div>
                         <div class="d-flex gap-2 justify-content-center mt-4 mb-2">
-                            <button type="button" class="btn w-sm btn-light" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn w-sm btn-light" data-bs-dismiss="modal">Fechar</button>
                             <button type="button" class="btn w-sm btn-danger" id="delete-notification">Yes, Delete
                                 It!</button>
                         </div>
