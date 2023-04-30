35d0949697b6b0309c4a96c69ac6a8cf9cb89d54
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-04-30 23:43:49 +0200

    Some updates in system

--- erp_cultos-app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ erp_cultos-app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -20,7 +20,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        Alert::success('Bem Vindo!');
+        Alert::success('Bem Vindo ao Sistema!');
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
