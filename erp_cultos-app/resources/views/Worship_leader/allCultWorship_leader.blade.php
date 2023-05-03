647681ebe93576577714fd51af2f3da6cddd5932
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-05-03 20:31:03 +0200

    Some updates in allCultWorship_leader

---
+++ erp_cultos-app/resources/views/Worship_leader/allCultWorship_leader.blade.php
@@ -0,0 +1,12 @@
+@extends('Layout.Index')
+@section('content')
+    @role('worship_leader')
+
+    {{--Inicio da parte contendo o conteudo da parte de todos os cultos--}}
+    
+    
+
+    {{--Fim da parte contendo todos os cultos--}}
+
+    @endrole
+@endsection
