5a2a74699c763f54f8aefd1574eb536b8b510f48
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-05-03 20:17:31 +0200

    Some updates in system

---
+++ erp_cultos-app/resources/views/Shepherd/allCultShepherd.blade.php
@@ -0,0 +1,13 @@
+@extends('Layout.Index')
+
+@section('content')
+    @role('shepherd')
+
+    {{--Inicio da parte contendo todo o conteudo da parte de cultos--}}
+
+    
+
+    {{--Fim da parte contendo todo o conteudo da parte de cultos--}}
+
+    @endrole
+@endsection
