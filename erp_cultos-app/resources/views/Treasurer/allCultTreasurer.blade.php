0f61da4eb67ff11bba356d563362c2e510dd4d7d
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-05-03 20:41:20 +0200

    Some updates in allCultTreasure

---
+++ erp_cultos-app/resources/views/Treasurer/allCultTreasurer.blade.php
@@ -0,0 +1,11 @@
+@extends('Layout.Index')
+
+@section('content')
+    @role('treasurer')
+
+    {{--Inicio da parte contendo o conteudo do sistema--}}
+
+    {{--Fim da parte contendo o conteudo do sistema--}}
+
+    @endrole
+@endsection
