5f66cc25561ec3fcf291e3848ee9429135b256c8
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-05-04 10:01:43 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Shepherd/addActivity.blade.php
+++ erp_cultos-app/resources/views/Shepherd/addActivity.blade.php
@@ -5,8 +5,87 @@
     @role('shepherd')
 
     {{--Inicio da parte contendo o formulario para adicionar a actividade--}}
+    
+        <form action="{{ route('storeActivity') }}" method="post">
+            @csrf
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header align-items-center d-flex">
+                            <h4 class="card-title mb-0 flex-grow-1">Dados da Actividade</h4>
+                        </div><!-- end card header -->
+                        <div class="card-body">
+                            <div class="live-preview">
+                                <div class="row gy-4">
+                                    <div class="col-xxl-6 col-md-6">
+                                        <div>
+                                            <label for="basiInput" class="form-label">Titulo</label>
+                                            <input type="text" class="form-control" id="basiInput" name="Title"
+                                                placeholder="Digite o titulo da actividade" required>
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-xxl-6 col-md-6">
+                                        <div>
+                                            <label for="labelInput" class="form-label">Horario</label>
+                                            <input type="time" class="form-control" id="labelInput" name="Hour" required>
+                                        </div>
+                                    </div>
+                                    <!--end col-->
+                                    <div class="col-xxl-6 col-md-6">
+                                        <div>
+                                            <label for="labelInput" class="form-label">Dia</label>
+                                            <input type="date" class="form-control" id="labelInput" name="Day" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-6 col-md-6">
+                                        <div>
+                                            <label for="labelInput" class="form-label">Departamento</label>
+                                            <select class="form-control" name="Group" required>
+                                                <option>...</option>
+                                                @foreach ($departments as $department)
+                                                    <option value="{{$department->id}}">{{$department->Name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-6 col-md-6">
+                                        <div>
+                                            <label for="labelInput" class="form-label">Responsavel</label>
+                                            <select class="form-control" name="Id_user" required>
+                                                <option>...</option>
+                                                @foreach ($users as $user)
+                                                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--end row-->
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="card">
+
+                        {{-- Inicio da parte de butoes --}}
+
+                        <button type="submit" name="submit" class="form-control bg-primary"
+                            style="color:white">Adicionar</button>
+
+                        {{-- Fim da parte de butoes --}}
+
+                    </div>
+                </div>
+            </div>
+        </form>
 
     {{--Fim do formulario para adicionar a actividade--}}
 
