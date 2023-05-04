7c857e17e721a9d75be43e7423f189fe85eee23b
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-05-04 10:01:01 +0200

    Some updates in system

---
+++ erp_cultos-app/resources/views/Shepherd/addActivity.blade.php
@@ -0,0 +1,15 @@
+@extends('Layout.Index')
+
+@section('content')
+
+    @role('shepherd')
+
+    {{--Inicio da parte contendo o formulario para adicionar a actividade--}}
+
+
+
+    {{--Fim do formulario para adicionar a actividade--}}
+
+    @endrole
+
+@endsection
