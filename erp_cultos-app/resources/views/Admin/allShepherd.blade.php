acd7d899a59e3752e4f789c916450fa612b17de8
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-05-07 02:34:25 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Admin/allShepherd.blade.php
+++ erp_cultos-app/resources/views/Admin/allShepherd.blade.php
@@ -42,33 +42,20 @@
                     <div class="card-body">
                         <div id="customerList">
                             <div class="card-body border border-dashed border-end-0 border-start-0">
-                                <form action="" method="post">
+                                <form action="{{route('searchShepherd')}}" method="post">
                                     @csrf
                                     <div class="row g-3">
-                                        <!--end col-->
-                                        {{--<div class="col-xxl-2 col-sm-6">
-                                            <div>
-                                                <input type="text" class="form-control" data-provider="flatpickr" data-date-format="d M, Y" data-range-date="true" id="demo-datepicker" placeholder="Select date">
-                                            </div>
-                                        </div>--}}
-                                        <!--end col-->
                                         <div class="col-xxl-4 col-sm-6">
                                             <div>
-                                                <select class="form-control" data-choices data-choices-search-false name="Department" id="idStatus">
+                                                <select class="form-control" data-choices data-choices-search-false name="theological_level" id="idStatus">
                                                     <option>...</option>
+
+                                                    @foreach ($users as $user)
+                                                        <option value="{{$user->theological_level}}">{{$option->theological_level}}</option>
+                                                    @endforeach
                                                 </select>
                                             </div>
                                         </div>
-                                        <!--end col-->
-                                        <div class="col-xxl-4 col-sm-6">
-                                            <div>
-                                                <select class="form-control" data-choices data-choices-search-false name="Day" id="idPayment">
-                                                    <option>...</option>
-                                                    <option></option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <!--end col-->
                                         <div class="col-xxl-1 col-sm-4">
                                             <div>
                                                 <button type="button" name="submit" class="btn btn-primary w-100" onclick="SearchData();"> <i class="ri-equalizer-fill me-1 align-bottom"></i>
