4c0827d3ff3c9ca6b1a94a3a1cfe707dcf09b32d
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-05-09 21:26:43 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Layout/Another.blade.php
+++ erp_cultos-app/resources/views/Layout/Another.blade.php
@@ -40,19 +40,19 @@
                         <div class="navbar-brand-box horizontal-logo">
                             <a href="index.html" class="logo logo-dark">
                                 <span class="logo-sm">
-                                    <img src="/assets/images/logo-sm.png" alt="" height="22">
+                                    <img src="/Ficheiros/a1.png" alt="" height="22">
                                 </span>
                                 <span class="logo-lg">
-                                    <img src="/assets/images/logo-dark.png" alt="" height="17">
+                                    <img src="/Ficheiros/a1.png" alt="" height="17">
                                 </span>
                             </a>
 
                             <a href="index.html" class="logo logo-light">
                                 <span class="logo-sm">
-                                    <img src="/assets/images/logo-sm.png" alt="" height="22">
+                                    <img src="/Ficheiros/a1.png" alt="" height="22">
                                 </span>
                                 <span class="logo-lg">
-                                    <img src="/assets/images/logo-light.png" alt="" height="17">
+                                    <img src="/Ficheiros/a1.png" alt="" height="17">
                                 </span>
                             </a>
                         </div>
