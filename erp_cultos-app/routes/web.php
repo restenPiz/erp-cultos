5e29faeecc2fc174407ecbe52f217487ac8bf7be
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-05-12 06:33:00 +0200

    Some updates in system

--- erp_cultos-app/routes/web.php
+++ erp_cultos-app/routes/web.php
@@ -198,6 +198,9 @@ Route::group(['prefix' => 'worship_leader', 'middleware' => ['role:worship_leade
 //Inicio das rotas da parte de tesoureiro treasurer
 Route::group(['prefix' => 'treasurer', 'middleware' => ['role:treasurer']], function () {
     
+    //Inicio da rota de perfil de usuario
+    Route::get('/ProfileTreasurer/{id}', [dashboardController::class, 'ProfileTreasurer'])->middleware(['auth'])->name('ProfileTreasurer');
+
     //Inicio da rota de show do comunicado
     Route::get('/showAnnouncementTreasurer/{id}', [dashboardController::class, 'showAnnouncementTreasurer'])->middleware(['auth'])->name('showAnnouncementTreasurer');
 
