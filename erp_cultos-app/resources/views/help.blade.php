72ff9b7df7109c07c7bc27c83d61727cac10b00a
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-05-16 14:02:03 +0200

    Some updates in system

---
+++ erp_cultos-app/resources/views/help.blade.php
@@ -0,0 +1,8 @@
+@extends('Layout.Principal')
+@section('content')
+
+    {{--Inicio da seccao contendo o conteudo do meu help us--}}
+
+    {{--Fim da seccao contendo o conteudo do meu help--}}
+
+@endsection
