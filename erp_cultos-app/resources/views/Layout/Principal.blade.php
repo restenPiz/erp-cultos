dc475445bb74fee00a7652b3cefd39f7f87509ce
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-05-16 14:10:09 +0200

    Some updates in system

--- erp_cultos-app/resources/views/Layout/Principal.blade.php
+++ erp_cultos-app/resources/views/Layout/Principal.blade.php
@@ -244,7 +244,7 @@
                                 <a class="dropdown-item" href="{{route('ProfileAdmin',['id'=>Auth::user()->id])}}"><i
                                         class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span
                                         class="align-middle">Perfil</span></a>
-                                <a class="dropdown-item" href="pages-faqs.html"><i
+                                <a class="dropdown-item" href="{{route('helpAdmin')}}"><i
                                         class="mdi mdi-lifebuoy text-muted fs-16 align-middle me-1"></i> <span
                                         class="align-middle">Ajuda</span></a>
                                 <div class="dropdown-divider"></div>
